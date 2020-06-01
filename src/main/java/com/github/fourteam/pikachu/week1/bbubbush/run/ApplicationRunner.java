8ac97970abfcb5ddfab5fdbdc2b3840b88316bec
unknown <sinsa1450@gmail.com>
2020-06-01 20:51:05 +0900

    개행 추가 유지니니니니니2

--- src/main/java/com/github/fourteam/pikachu/week1/bbubbush/run/ApplicationRunner.java
+++ src/main/java/com/github/fourteam/pikachu/week1/bbubbush/run/ApplicationRunner.java
@@ -4,6 +4,8 @@ import com.github.fourteam.pikachu.week1.bbubbush.domain.Product;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
+
+
         Product product = new Product.Builder()
                 .productCode(1L)
                 .productPrice(1000L)
